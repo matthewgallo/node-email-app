43b9e6d4fa67a4eb5867fb74de9546538b6a4de2
Matt Gallo <mdgallo@us.ibm.com>
2019-10-10 08:39:02 -0400

    Initial commit.

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+// setup view
+app.engine('.hbs', handlebars({
+	defaultLayout: 'layout',
+	extname: '.hbs',
+	layoutsDir: `${__dirname}/views/layouts/`
+}));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', '.hbs');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(__dirname + '/public/js/client.js'));
+
+// Body parser.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'Nodemailer demo app',
+		msg: ''
+	});
+});
+
+app.post('/send', (req, res) => {
+	console.log(req.body);
+	console.log(process.env.EMAIL_ADDRESS);
+	const output = `
+		<h2>Yay, we sent an email from our web app!</h2>
+		<h3>Message</h3>
+		<p>${req.body.emailMessageValue}</p>
+	`;
+
+	// create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+		service: 'gmail',
+		port: 25,
+		secure: false,
+		requireTLS: true,
+        auth: {
+            user: `${process.env.EMAIL_ADDRESS}`,
+            pass: `${process.env.EMAIL_PASSWORD}`
+		},
+		tls: {
+			rejectUnauthorized: false
+		}
+	});
+	
+	let mailOptions = {
+		from: '"Nodemail test" <mdgallo07@gmail.com>',
+        to: 'mdgallo07@gmail.com',
+        subject: 'Hello ✔',
+        html: output
+	}
+
+	transporter.sendMail(mailOptions, (error, response) => {
+		console.log(response);
+		if (error) {
+			console.log(error);
+		}
+		if (response) {
+			console.log('*********************** SUCCESSSSSSSSSS! ***********************');
+			res.render('index', {msg: 'Email has been sent.'});
+		}
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server started.');
+})
---
+++ public/js/client.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+	const submitButton = document.querySelectorAll('#submit-email-button');
+	const emailMessage = document.querySelectorAll('#email-message-text-area');
+	if (submitButton) {
+		submitButton[0].addEventListener('click', function (event) {
+			event.preventDefault();
+			const data = {
+				emailMessageValue: emailMessage[0].value,
+			}
+			(async () => {
+				const rawResponse = await fetch('/send', {
+				  method: 'POST',
+				  headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				  },
+				  body: JSON.stringify(data)
+				});
+				const content = await rawResponse;
+			  
+				console.log(content);
+				if (content.status === 200) {
+					const emailForm = document.querySelectorAll('#email-form');
+					const appContainer = document.querySelectorAll('#app-container');
+					const successMessageElement = document.createElement("P");
+					const successMessage = document.createTextNode("Congratulations, your email sent successfully!");
+					successMessageElement.appendChild(successMessage);
+					appContainer[0].appendChild(successMessageElement);   
+					emailForm[0].style.display = 'none';
+				}
+			  })();
+		});
+	}
+});
